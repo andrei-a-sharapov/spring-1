08a5318dc5e72c2ec887349d3280ee061a03dbd0
Andrei Sharapov <andrei.a.sharapov@gmail.com>
2018-11-25 23:27:25 +0300

    Use LegacyApp to populate an app instance with sample data

--- src/main/java/lab1/Application.java
+++ src/main/java/lab1/Application.java
@@ -1,12 +1,22 @@
 package lab1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  private void afterStartup() {
+    LegacyApp.main(new String[]{});
   }
 }
